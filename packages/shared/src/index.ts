66ea3aa7c48f3a6d5b00066cee03ae1bd59142a2
Juan Miguel Carrasquilla <jmcarrasquilla@uninorte.edu.co>
2026-09-09 21:42:22 -0500

    Creacion inical del Backend

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,14 @@
+﻿export interface Player {
+  id: string;
+  name: string;
+  team?: 'A' | 'B';
+  isHost: boolean;
+}
+
+export type GameMode = 'LOBBY' | 'MIMIRETO' | 'POPSAUCE';
+
+export interface RoomState {
+  code: string;
+  gameMode: GameMode;
+  players: Player[];
+}
